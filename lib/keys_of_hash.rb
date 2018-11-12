2446f667058c32318353113753dbe3ed1d538a50
Roman Pirag <mr.anadale@yahoo.com>
2018-11-12 01:31:04 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arrgs) 
+    new_array = []
+    arrgs.map do |key,value|
+      if value
+        new_array << key
+      else 
+        return "wat" 
+      end
+    end 
+  new_array.compact 
   end
 end
