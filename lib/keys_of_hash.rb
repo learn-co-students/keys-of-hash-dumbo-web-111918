ecd8d993c24ef9c81621fb6b777cbf4cd3bfd366
Jonathan Biro <jonnybrx@gmail.com>
2018-11-14 04:08:29 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
+require 'pry'
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    keys = []
+    self.each do |k,v|
+      if arguments.include? v
+        keys.push k # or instead of push you can use <<
+      end
+    end
+    return keys
   end
 end
+
+
+#look into using filter, or map and compact
